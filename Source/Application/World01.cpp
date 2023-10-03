f50815f2921f857303c9b9f6fb589d82a1bcbd74
NeuHezxp <mpatino@student.neumont.edu>
2023-10-03 11:02:33 -0600

    Add project files.

---
+++ Source/Application/World01.cpp
@@ -0,0 +1,39 @@
+#include "World01.h"
+#include "Framework/Framework.h"
+#include "Renderer/Renderer.h"
+#include "Input/InputSystem.h"
+
+namespace nc
+{
+    bool World01::Initialize()
+    {
+        return true;
+    }
+
+    void World01::Shutdown()
+    {
+    }
+
+    void World01::Update(float dt)
+    {
+    }
+
+    void World01::Draw(Renderer& renderer)
+    {
+        // pre-render
+        renderer.SetColor(0, 0, 0, 0);
+        renderer.BeginFrame();
+
+        // render
+        renderer.SetColor(random(255), (random(255)), 255, 255);
+        renderer.DrawPoint(random(renderer.GetWidth()), random(renderer.GetHeight()));
+
+        for (int i = 0; i < 1000; ++i)
+        {
+            renderer.DrawPoint(random(renderer.GetWidth()), random(renderer.GetHeight()));
+        }
+
+        // post-render
+        renderer.EndFrame();
+    }
+}
